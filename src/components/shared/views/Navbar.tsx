110e16a0911727957a96a0065b4979dccbf29bdd
AhmerAli17 <ahmerali1771@gmail.com>
2023-09-20 18:10:08 +0100

    update

--- src/components/shared/views/Navbar.tsx
+++ src/components/shared/views/Navbar.tsx
@@ -1,4 +1,8 @@
-
+"use client"
+import {MdClose} from "react-icons/md"
+import {GiHamburgerMenu} from "react-icons/Gi"
+import {useState} from "react"
+import Mobile from '@/components/all functions/Navbar/item/mobile';
 import {BsCart2  } from 'react-icons/bs';
 import { BiSearchAlt2 } from 'react-icons/bi';
  import { NavbarArray, NavbarItemType } from "@/components/all functions/Navbar/item/Arry"
@@ -7,17 +11,20 @@ import Link from "next/link"
 import {HiOutlineChevronDown } from 'react-icons/hi';
 import DropDown from './DropDown';
 const Navbar = () =>{
+    const [isNavbaropen, setisNavbaropen] = useState<boolean> (false)
     return (
-        <div className="py-2  flex items-center justify-between "> 
-            <div className="w-36 cursor-pointer ">
+        <div>
+        <div className="py-2  flex items-center justify-between space-x-12 "> 
+            <div className=" flex-shrink-0 w-36 cursor-pointer ">
             <Image width={500} height={500} src={"/Logo.webp"} alt="Logo" />
             </div>
-            <ul className="flex  items-center space-x-7   font-semibold text-lg text-blue-600 relative  ">
+            <div className='hidden lg:flex items-center justify-between w-full'>
+            <ul className="flex  items-center space-x-6   font-semibold text-lg text-blue-600 relative  ">
                 {NavbarArray.map((item: NavbarItemType, index:number)=>(
-                  <li key={index} className='flex items-center  hover:bg-gray-100 group  '>
+                  <li className='flex items-center  hover:bg-gray-100 group '>
                     <Link href={item.href}> {item.label}</Link>
-                    {item.isDropdown?<HiOutlineChevronDown className='mt-1 -rotate-180 group-hover:rotate-0  duration-300 hover:cursor-pointer' size={16}/>:""}
-                    { item.isDropdown && <div className=' invisible group-hover:visible absolute gap-1 top-6   py-2 px-4 text-lg min-w-[6.5rem] border bg-gray-100 font-light'>
+                    {item.isDropdown?<HiOutlineChevronDown className='mt-1 -rotate-180 group-hover:rotate-0  hover:duration-300 hover:cursor-pointer' size={16}/>:""}
+                    { item.isDropdown && <div className=' invisible group-hover:visible absolute gap-1 top-6   py-2 px-4 text-lgn min-w-[6.5rem] border bg-gray-100 font-light'>
                   
                     <DropDown item={item}/>
                     </div>}
@@ -31,15 +38,31 @@ const Navbar = () =>{
                 <input
                 type="text"
                 className="outline-none pl-1 pr-5 py-1 w-80 "
-                placeholder="Search our pruduct"
+                placeholder="Search our pruduct" 
                 /> 
                     </div>
-                <div className=' relative w-11 h-11 rounded-full bg-gray-300 flex items-center justify-center'>
+                <div className=' flex-shrink-0 relative w-11 h-11 rounded-full bg-gray-300 flex items-center justify-center'>
                     <div className='absolute top-0 bg-red-500 rounded-full w-4 h-4 flex  items-center justify-center right-1' >1</div>
                  <BsCart2 size={20}/>
 
                 </div>
-        </div>
+                </div>
+                <div onClick={()=> setisNavbaropen(!isNavbaropen)}> 
+      { isNavbaropen? 
+      <div className='flex lg:hidden'>
+      <MdClose size={30}/>
+</div>
+  :
+        <div className='flex lg:hidden'>
+    <GiHamburgerMenu size={25}/>
+    </div>
+}
+</div>
+ </div>
+  { 
+    isNavbaropen && <Mobile/>               
+    }
+    </div>
     )
 }
 export default Navbar 
