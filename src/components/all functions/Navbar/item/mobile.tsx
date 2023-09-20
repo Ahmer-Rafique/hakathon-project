c316ccc130de5f1634b4761cdc1119d59e30667a
AhmerAli17 <ahmerali1771@gmail.com>
2023-09-20 18:33:59 +0100

    update

--- src/components/all functions/Navbar/item/mobile.tsx
+++ src/components/all functions/Navbar/item/mobile.tsx
@@ -11,13 +11,13 @@ const Mobile = () => {
           {
             NavbarArray.map((item:NavbarItemType , index:number )=>{
                 return(
-                    <div>
+                    
                     <li key={index} className="py-2 px-3 hover:bg-purple-600 rounded-md duration-300 list-none flex justify-between ">
                       <Link href={item.href}>{item.label}</Link>
                       {item.isDropdown?<HiOutlineChevronDown className='mt-1 -rotate-180 group-hover:rotate-0  hover:duration-300 hover:cursor-pointer' size={16}/>:""}
                     
                     </li>
-                    </div>
+                    
                 )
             })
           }
