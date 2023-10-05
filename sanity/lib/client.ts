9b305e73a8675b626a9dcbdd54a524803f9acf6b
AhmerAli17 <ahmerali1771@gmail.com>
2023-10-05 18:06:21 +0100

    update

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -4,5 +4,7 @@ import { apiVersion, dataset, projectId, useCdn } from '../env'
 
 export const client = createClient({
     projectId:`${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}`,
-    dataset:`${process.env.NEXT_PUBLIC_SANITY_DATASET}`
+    dataset:`${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
+    useCdn:false,
+    apiVersion:"v2023-09-28"
 });
